913596e3169af90c3881ce56540e98202f663c0b
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2021-06-01 18:19:27 +0200

    remove debug

--- models/chat/list.go
+++ models/chat/list.go
@@ -1,20 +1,16 @@
 package chat
 
 import (
-	"fmt"
-
 	sq "github.com/Masterminds/squirrel"
 )
 
 // List lists chats
 func (m *Model) List(vaccineName string) ([]*Chat, error) {
-	fmt.Println("vaccinename:", vaccineName)
 	q := m.getSelectBuilder().Where(
 		sq.Eq{"enabled": true}).
 		Where(
 			sq.Or{sq.Like{"filters": "%" + vaccineName + "%"}, sq.Eq{"filters": nil}},
 		)
-	fmt.Println(q.ToSql())
 
 	rows, err := q.Query()
 	if err != nil {
