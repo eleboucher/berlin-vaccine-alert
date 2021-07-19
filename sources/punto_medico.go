85e6f2262d759e27d6cc664aa311ccc29934494b
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2021-07-19 18:04:07 +0200

    log the body

--- sources/punto_medico.go
+++ sources/punto_medico.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/eleboucher/covid/vaccines"
+	"github.com/sirupsen/logrus"
 )
 
 const tPunto = "{{.Nr}} appointments for {{.Name}} available https://punctum-medico.de/onlinetermine/"
@@ -56,6 +57,7 @@ func (p *PuntoMedico) Fetch() ([]*vaccines.Result, error) {
 	if err != nil {
 		return nil, err
 	}
+	logrus.Error(string(body))
 	var resp TMessage
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
