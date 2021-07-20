4022123299a0a04b7781b95100fbda2bc4b563df
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2021-07-20 10:13:22 +0200

    disable doctolib again

--- main.go
+++ main.go
@@ -141,7 +141,7 @@ func main() {
 	}
 	chatModel := chat.NewModel(db)
 	telegram := NewBot(bot, chatModel)
-	doctolibs, err := getAllDoctolibSources()
+	// doctolibs, err := getAllDoctolibSources()
 	if err != nil {
 		log.Error(err)
 		return
@@ -155,7 +155,7 @@ func main() {
 		&sources.Helios{},
 	}
 
-	s = append(s, doctolibs...)
+	// s = append(s, doctolibs...)
 
 	var runCMD = &cobra.Command{
 		Use:   "run",
