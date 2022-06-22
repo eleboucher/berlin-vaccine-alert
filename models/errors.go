b9cbc6ab13963dea2467775fd406c832cc9980ed
Siavash Salarzehi <51620597+siasalar@users.noreply.github.com>
2022-06-22 10:43:56 +0200

    Delete errors.go

--- models/errors.go
+++
@@ -1,7 +0,0 @@
-package models
-
-var (
-	// PqUniqueViolationErrName is the name of the error code returned
-	// by pq when a given record violates a unique constraint on a table
-	PqUniqueViolationErrName = "unique_violation"
-)
